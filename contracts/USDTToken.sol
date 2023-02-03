c256041059af17d05c39cb6904fa91e42c714118
Ariel Jaimez <arieljaimez@gmail.com>
2023-02-03 13:41:48 -0300

    Fix: L01. Floating Pragma

--- contracts/USDTToken.sol
+++ contracts/USDTToken.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
+
 // import "../../openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 interface IERC20 {
     /**
