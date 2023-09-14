88bc9dac59b7137c9796527e67bb60667c538eb3
Ariel Jaimez <arieljaimez@gmail.com>
2023-09-14 10:16:10 -0300

    Se agrego detalles de comandos.

--- scripts/Paydece/deploy_add_stables.js
+++ scripts/Paydece/deploy_add_stables.js
@@ -1,5 +1,5 @@
 async function main() {
-  const address = "0xDb1c6c9B11f4c63b87bd81E642e9840fc1F2ADAe"; //Address Escrow
+  const address = "0xd4a5Bb96BDE96D2cE624101D40c0ffC1aA2C98E8"; //Address Escrow
 
   const accounts = await ethers.getSigners();
 
@@ -9,19 +9,48 @@ async function main() {
     accounts[3]
   );
 
+  console.log(accounts[3].address);
   console.log(await escrow.version());
 
-  // BSC TESTNET
+  // POLYGON MAINNET
+  // await escrow.addStablesAddresses(
+  //   "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
+  // );
+
+  // await escrow.addStablesAddresses(
+  //   "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"
+  // );
+
+  // await escrow.addStablesAddresses(
+  //   "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
+  // );
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // BSC MAIN
+  // await escrow.addStablesAddresses(
+  //   "0x55d398326f99059fF775485246999027B3197955"
+  // );
+
+  // await escrow.addStablesAddresses(
+  //   "0x91bc956F064d755dB2e4EfE839eF0131e0b07E28"
+  // );
+
+  // await escrow.addStablesAddresses(
+  //   "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3"
+  // );
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // ETHE MAIN
   await escrow.addStablesAddresses(
-    "0x07865c6E87B9F70255377e024ace6630C1Eaa37F"
+    "0x6b175474e89094c44da98b954eedeac495271d0f"
   );
 
   await escrow.addStablesAddresses(
-    "0xf9d137CaEcebC8384196fBe902884F9916c63757"
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
   );
 
   await escrow.addStablesAddresses(
-    "0x9DB70bE165d33E263861265477cC34cdD6106DCb"
+    "0xdAC17F958D2ee523a2206206994597C13D831ec7"
   );
 
   //MUMBAY POLYGON
